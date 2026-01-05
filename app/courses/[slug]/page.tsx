952dac7d5550d61d5ce67c344d2b1447a55df01a
Qal Tech <130528093+qal-technologies@users.noreply.github.com>
2026-01-05 22:01:38 +0100

    Update page.tsx

--- app/courses/[slug]/page.tsx
+++ app/courses/[slug]/page.tsx
@@ -5,7 +5,8 @@ import VideoPlayer from "@/components/courses/VideoPlayer";
 import ResourceDownloader from "@/components/courses/ResourceDownloader";
 import { Box, Heading, VStack, Tab, TabList, TabPanel, Tabs } from "@chakra-ui/react";*/
 
-import {View, Text} from 'react-native';
+/*import {View, Text} from 'react-native';*/
+
 
 export default function CoursePage() {
   
@@ -16,47 +17,8 @@ export default function CoursePage() {
     assignments: [],
     tests: [],
   };
+
   return (
-    {/*<CourseLayout>
-      <VStack spacing={8} align="stretch">
-        <Heading as="h1" size="2xl">
-          {course.title}
-        </Heading>
-        <VideoPlayer src={course.videoSrc} />
-        <Tabs>
-            <TabList>
-                <Tab>Resources</Tab>
-                <Tab>Assignments</Tab>
-                <Tab>Tests</Tab>
-            </TabList>
-            
-<TabPanels>
-                <TabPanel>
-                    <ResourceDownloader resources={course.resources} />
-                </TabPanel>
-                <TabPanel>
-                    {course.assignments.map(assignment => (
-                        <Box key={assignment.title}>
-                            <Heading size="md">{assignment.title}</Heading>
-                            <p>{assignment.content}</p>
-                        </Box>
-                    ))}
-                </TabPanel>
-                <TabPanel>
-                {course.tests.map(test => (
-                        <Box key={test.title}>
-                            <Heading size="md">{test.title}</Heading>
-                            <p>{test.content}</p>
-                        </Box>
-                    ))}
-                </TabPanel>
-            </TabPanels>
-
-        </Tabs>
-      </VStack>
-    </CourseLayout>
-*/}
-
-<div><p>Course slug</p></div>
+       <p>Course slug</p>
   );
 }
